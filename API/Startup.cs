18b150d9ef6bc266cd3d56fdf5c1b78b771baa62
Jon Thornton <jon.thornton07@gmail.com>
2020-05-04 19:36:52 -0400

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,9 +27,16 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(opt => 
+            services.AddDbContext<DataContext>(opt =>
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
             });
             services.AddControllers();
         }
@@ -43,6 +50,7 @@ namespace API
             }
 
             // app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
             app.UseRouting();
             app.UseAuthorization();
             app.UseEndpoints(endpoints =>
---
+++ client-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Values from "./Values";
+
+function App() {
+  return (
+    <div>
+      <Values />
+    </div>
+  );
+}
+
+export default App;
