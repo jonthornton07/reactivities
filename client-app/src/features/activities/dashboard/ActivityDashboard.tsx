97990dc4b97bfe40260cbc0549ab82e04868f0db
Jon Thornton <jon.thornton07@gmail.com>
2020-09-16 18:27:45 -0400

    Section 21: Adding load more

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,27 +1,53 @@
-import React, { useEffect, useContext } from "react";
-import { Grid } from "semantic-ui-react";
+import React, { useEffect, useContext, useState } from "react";
+import { Button, Grid, Loader } from "semantic-ui-react";
 import ActivityList from "./ActivityList";
 import { LoadingComponent } from "../../../app/layout/LoadingComponent";
 import { observer } from "mobx-react-lite";
 import { RootStoreContext } from "../../../app/stores/rootStore";
+import InfiniteScroll from "react-infinite-scroller";
 
 const ActivityDashboard = () => {
   const rootStore = useContext(RootStoreContext);
-  const { loadActivities, loading } = rootStore.activityStore;
+  const {
+    loadActivities,
+    loading,
+    setPage,
+    page,
+    totalPages,
+  } = rootStore.activityStore;
+
+  const [loadingNext, setLoadingNext] = useState(false);
+
+  const handlelGetNext = () => {
+    setLoadingNext(true);
+    setPage(page + 1);
+    loadActivities().then(() => setLoadingNext(false));
+  };
 
   useEffect(() => {
     loadActivities();
   }, [loadActivities]);
 
-  if (loading) return <LoadingComponent content="Loading Activities..." />;
+  if (loading && page == 0)
+    return <LoadingComponent content="Loading Activities..." />;
 
   return (
     <Grid>
       <Grid.Column width={10}>
-        <ActivityList />
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={handlelGetNext}
+          hasMore={!loadingNext && page + 1 < totalPages}
+          initialLoad={false}
+        >
+          <ActivityList />
+        </InfiniteScroll>
       </Grid.Column>
       <Grid.Column width={6}>
         <h1>Activity Filters</h1>
+      </Grid.Column>
+      <Grid.Column width={10}>
+        <Loader active={loadingNext} />
       </Grid.Column>
     </Grid>
   );
